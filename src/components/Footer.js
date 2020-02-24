f8c51c6a27d0acd005c3337717c8783ce6f9e329
Muchengeti <matindemv@outlook.com>
2020-02-24 14:52:22 +1300

    footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,20 +7,19 @@ import Col from 'react-bootstrap/Col'
 class Footer extends Component {
   render() {
     return (
-        <div className="text-center" style={{backgroundColor:"black", color:"white"}}>
-            {/* <h2>Friendly, helpful assistance with no obligation</h2> */}
+        <div className="text-center" style={{backgroundColor:"slategray", color:"white"}}>
             <br/>
             <Container>
                 <Row>
                 <Col xs={4}>
-                    <h5>Location</h5>
+                    <h4>Location</h4>
                     <hr style={{backgroundColor:"white"}}/>
-                    <p>101A Freyberg Street</p>
-                    <p>Lyall Bay</p>
+                    <p>101A Freyberg Street,</p>
+                    <p>Lyall Bay,</p>
                     <p>Wellington 6022</p>
                 </Col>
                 <Col xs={4}>
-                    <h5>Hours</h5>
+                    <h4>Hours</h4>
                     <hr style={{backgroundColor:"white"}}/>
                     <p>Monday 7:30 am - 6:00 pm</p>
                     <p>Tuesday 7:30 am - 6:00 pm</p>
@@ -31,20 +30,24 @@ class Footer extends Component {
                     <p>Sunday closed</p>
                 </Col>
                 <Col xs={4}>
-                    <h5>Quick Links</h5>
+                    <h4>Quick Links</h4>
                     <hr style={{backgroundColor:"white"}}/>
-                    <a href="/">Home</a>
+                    <a href="/" style={{color:"white"}}>Home</a>
                     <br/>
                     <br/>
-                    <a href="/">About</a>
+                    <a href="/About" style={{color:"white"}}>About</a>
                     <br/>
                     <br/>
-                    <a href="/">Contact</a>
+                    <a href="/Contact" style={{color:"white"}} >Contact</a>
                 </Col>
                 </Row>
-            <p>© 2020 . A1 Driving School Wellington . All Rights Reserved .</p>
             </Container>
+            <div style={{backgroundColor:"dimgray"}}>
+                <br/>
+                <p style={{margin:0}}>© 2020 | A1 Driving School Wellington | All Rights Reserved .</p>
+                <br/>
             </div>
+        </div>
     );
   }
 }
